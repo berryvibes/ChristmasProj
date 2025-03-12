3e1e34b33782113b77b1ac9624fa8e42a6b441c3
berryvibes <bolawoleayo@gmail.com>
2025-03-13 00:51:06 +0100

    Added wsgis

--- christmasProj/christmasProj/wsgi.py
+++ christmasProj/christmasProj/wsgi.py
@@ -8,8 +8,14 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+
 
 from django.core.wsgi import get_wsgi_application
+
+# Add the project root directory to sys.path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'christmasProj.settings')
 
