7942f86b11d3c2f0cd85fc1b50c72cd530fb71f5
berryvibes <bolawoleayo@gmail.com>
2025-03-12 23:34:01 +0100

    another commits

--- christmasProj/christmasProj/settings.py
+++ christmasProj/christmasProj/settings.py
@@ -87,17 +87,17 @@ WSGI_APPLICATION = 'christmasProj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-    
-}
-
 # DATABASES = {
-#     'default': dj_database_url.parse(env('DATABSE_URL'))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+    
 # }
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABSE_URL'))
+}
 
 
 
